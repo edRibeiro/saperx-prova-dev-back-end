5b705e2d29037fe916539ce2a371e91bbd7c54fc
edRibeiro <edersonribeiro.ita@gmail.com>
2024-03-24 22:10:26 -0300

    feat: Installed UI React Inertia package.

--- app/Services/ContactServiceInterface.php
+++ app/Services/ContactServiceInterface.php
@@ -4,10 +4,12 @@ namespace App\Services;
 
 use App\Models\Contact;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
 
 interface ContactServiceInterface
 {
   public function findAll(): LengthAwarePaginator;
+  public function findAllRaw(): Collection;
   public function findById(int $contactId): Contact;
   public function store(array $data): Contact;
   public function update(array $data, int $contactId): Contact;
